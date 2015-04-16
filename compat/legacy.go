472487261005a80cd94c232c6a6571a8fa31610c
Vytautas Šaltenis <vytas@rtfb.lt>
2015-04-16 22:54:02 +0300

    Fix build: make compat a separate package
    And make it refer back to blackfriday package.

--- compat/legacy.go
+++ compat/legacy.go
@@ -11,88 +11,84 @@
 // This file contains legacy constants to maintain backward compatibility. The
 // constants are all initialized to their new counterparts.
 //
-package blackfriday
+package compat
 
-const VERSION = Version
+import (
+	//"github.com/russross/blackfriday"
+	".."
+)
+
+const VERSION = blackfriday.Version
 
 const (
-	EXTENSION_NO_INTRA_EMPHASIS          = int(NoIntraEmphasis)
-	EXTENSION_TABLES                     = int(Tables)
-	EXTENSION_FENCED_CODE                = int(FencedCode)
-	EXTENSION_AUTOLINK                   = int(Autolink)
-	EXTENSION_STRIKETHROUGH              = int(Strikethrough)
-	EXTENSION_LAX_HTML_BLOCKS            = int(LaxHTMLBlocks)
-	EXTENSION_SPACE_HEADERS              = int(SpaceHeaders)
-	EXTENSION_HARD_LINE_BREAK            = int(HardLineBreak)
-	EXTENSION_TAB_SIZE_EIGHT             = int(TabSizeEight)
-	EXTENSION_FOOTNOTES                  = int(Footnotes)
-	EXTENSION_NO_EMPTY_LINE_BEFORE_BLOCK = int(NoEmptyLineBeforeBlock)
-	EXTENSION_HEADER_IDS                 = int(HeaderIDs)
-	EXTENSION_TITLEBLOCK                 = int(Titleblock)
-	EXTENSION_AUTO_HEADER_IDS            = int(AutoHeaderIDs)
+	EXTENSION_NO_INTRA_EMPHASIS          = int(blackfriday.NoIntraEmphasis)
+	EXTENSION_TABLES                     = int(blackfriday.Tables)
+	EXTENSION_FENCED_CODE                = int(blackfriday.FencedCode)
+	EXTENSION_AUTOLINK                   = int(blackfriday.Autolink)
+	EXTENSION_STRIKETHROUGH              = int(blackfriday.Strikethrough)
+	EXTENSION_LAX_HTML_BLOCKS            = int(blackfriday.LaxHTMLBlocks)
+	EXTENSION_SPACE_HEADERS              = int(blackfriday.SpaceHeaders)
+	EXTENSION_HARD_LINE_BREAK            = int(blackfriday.HardLineBreak)
+	EXTENSION_TAB_SIZE_EIGHT             = int(blackfriday.TabSizeEight)
+	EXTENSION_FOOTNOTES                  = int(blackfriday.Footnotes)
+	EXTENSION_NO_EMPTY_LINE_BEFORE_BLOCK = int(blackfriday.NoEmptyLineBeforeBlock)
+	EXTENSION_HEADER_IDS                 = int(blackfriday.HeaderIDs)
+	EXTENSION_TITLEBLOCK                 = int(blackfriday.Titleblock)
+	EXTENSION_AUTO_HEADER_IDS            = int(blackfriday.AutoHeaderIDs)
 )
 
 const (
-	LINK_TYPE_NOT_AUTOLINK = int(LinkTypeNotAutolink)
-	LINK_TYPE_NORMAL       = int(LinkTypeNormal)
-	LINK_TYPE_EMAIL        = int(LinkTypeEmail)
+	LINK_TYPE_NOT_AUTOLINK = int(blackfriday.LinkTypeNotAutolink)
+	LINK_TYPE_NORMAL       = int(blackfriday.LinkTypeNormal)
+	LINK_TYPE_EMAIL        = int(blackfriday.LinkTypeEmail)
 )
 
 const (
-	LIST_TYPE_ORDERED           = int(ListTypeOrdered)
-	LIST_ITEM_CONTAINS_BLOCK    = int(ListItemContainsBlock)
-	LIST_ITEM_BEGINNING_OF_LIST = int(ListItemBeginningOfList)
-	LIST_ITEM_END_OF_LIST       = int(ListItemEndOfList)
+	LIST_TYPE_ORDERED           = int(blackfriday.ListTypeOrdered)
+	LIST_ITEM_CONTAINS_BLOCK    = int(blackfriday.ListItemContainsBlock)
+	LIST_ITEM_BEGINNING_OF_LIST = int(blackfriday.ListItemBeginningOfList)
+	LIST_ITEM_END_OF_LIST       = int(blackfriday.ListItemEndOfList)
 )
 
 const (
-	TABLE_ALIGNMENT_LEFT   = int(TableAlignmentLeft)
-	TABLE_ALIGNMENT_RIGHT  = int(TableAlignmentRight)
-	TABLE_ALIGNMENT_CENTER = int(TableAlignmentCenter)
+	TABLE_ALIGNMENT_LEFT   = int(blackfriday.TableAlignmentLeft)
+	TABLE_ALIGNMENT_RIGHT  = int(blackfriday.TableAlignmentRight)
+	TABLE_ALIGNMENT_CENTER = int(blackfriday.TableAlignmentCenter)
 )
 
 const (
-	TAB_SIZE_DEFAULT = TabSizeDefault
-	TAB_SIZE_EIGHT   = TabSizeDouble
+	TAB_SIZE_DEFAULT = blackfriday.TabSizeDefault
+	TAB_SIZE_EIGHT   = blackfriday.TabSizeDouble
 )
 
 const (
-	HTML_SKIP_HTML                 = int(SkipHTML)
-	HTML_SKIP_STYLE                = int(SkipStyle)
-	HTML_SKIP_IMAGES               = int(SkipImages)
-	HTML_SKIP_LINKS                = int(SkipLinks)
-	HTML_SAFELINK                  = int(Safelink)
-	HTML_NOFOLLOW_LINKS            = int(NofollowLinks)
-	HTML_NOREFERRER_LINKS          = int(NoreferrerLinks)
-	HTML_HREF_TARGET_BLANK         = int(HrefTargetBlank)
-	HTML_TOC                       = int(Toc)
-	HTML_OMIT_CONTENTS             = int(OmitContents)
-	HTML_COMPLETE_PAGE             = int(CompletePage)
-	HTML_USE_XHTML                 = int(UseXHTML)
-	HTML_USE_SMARTYPANTS           = int(UseSmartypants)
-	HTML_SMARTYPANTS_FRACTIONS     = int(SmartypantsFractions)
-	HTML_SMARTYPANTS_LATEX_DASHES  = int(SmartypantsLatexDashes)
-	HTML_SMARTYPANTS_ANGLED_QUOTES = int(SmartypantsAngledQuotes)
-	HTML_FOOTNOTE_RETURN_LINKS     = int(FootnoteReturnLinks)
+	HTML_SKIP_HTML                 = int(blackfriday.SkipHTML)
+	HTML_SKIP_STYLE                = int(blackfriday.SkipStyle)
+	HTML_SKIP_IMAGES               = int(blackfriday.SkipImages)
+	HTML_SKIP_LINKS                = int(blackfriday.SkipLinks)
+	HTML_SAFELINK                  = int(blackfriday.Safelink)
+	HTML_NOFOLLOW_LINKS            = int(blackfriday.NofollowLinks)
+	HTML_NOREFERRER_LINKS          = int(blackfriday.NoreferrerLinks)
+	HTML_HREF_TARGET_BLANK         = int(blackfriday.HrefTargetBlank)
+	HTML_TOC                       = int(blackfriday.Toc)
+	HTML_OMIT_CONTENTS             = int(blackfriday.OmitContents)
+	HTML_COMPLETE_PAGE             = int(blackfriday.CompletePage)
+	HTML_USE_XHTML                 = int(blackfriday.UseXHTML)
+	HTML_USE_SMARTYPANTS           = int(blackfriday.UseSmartypants)
+	HTML_SMARTYPANTS_FRACTIONS     = int(blackfriday.SmartypantsFractions)
+	HTML_SMARTYPANTS_LATEX_DASHES  = int(blackfriday.SmartypantsLatexDashes)
+	HTML_SMARTYPANTS_ANGLED_QUOTES = int(blackfriday.SmartypantsAngledQuotes)
+	HTML_FOOTNOTE_RETURN_LINKS     = int(blackfriday.FootnoteReturnLinks)
 )
 
 func MarkdownBasic(input []byte) []byte {
-	// set up the HTML renderer
-	htmlFlags := HTML_USE_XHTML
-	renderer := HtmlRenderer(htmlFlags, "", "")
-
-	// set up the parser
-	extensions := 0
-
-	return Markdown(input, renderer, extensions)
+	return blackfriday.MarkdownBasic(input)
 }
 
 func MarkdownCommon(input []byte) []byte {
-	// set up the HTML renderer
-	renderer := HtmlRenderer(commonHtmlFlags, "", "")
-	return Markdown(input, renderer, commonExtensions)
+	return blackfriday.MarkdownCommon(input)
 }
 
-func Markdown(input []byte, renderer Renderer, extensions int) []byte {
-	return nil
+func Markdown(input []byte, renderer blackfriday.Renderer, extensions int) []byte {
+	return blackfriday.Markdown(input, renderer, blackfriday.Extensions(extensions))
 }
