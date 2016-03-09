ef4299b10945ac3384a1fd8bffdcf861b7a2809b
Vytautas Šaltenis <vytas@rtfb.lt>
2016-03-09 10:56:14 +0200

    WIP: update some helper tests

--- ast_test.go
+++ ast_test.go
@@ -1,6 +1,7 @@
 package blackfriday
 
 import (
+	"strings"
 	"testing"
 )
 
@@ -31,15 +32,14 @@ func TestAST(t *testing.T) {
 		}
 	}
 }
-*/
 
 func TestAST2(t *testing.T) {
 	var tests = []string{
 		"# Header 1\n\n----\n\n> quote",
-		"<h1>Header 1</h1>\n\n<hr />\n\n<blockquote>\n<p>quote</p>\n</blockquote>\n",
+		"<h1>Header 1</h1>\n<hr />\n<blockquote>\n<p>quote</p>\n</blockquote>\n",
 
 		"# Header\n\n<div><span><em>plain html</em></span></div>",
-		"<h1>Header</h1>\n\n<div><span><em>plain html</em></span></div>\n",
+		"<h1>Header</h1>\n<div><span><em>plain html</em></span></div>\n",
 
 		"* List\n",
 		"<ul>\n<li>\n<p>List</p>\n</li>\n</ul>\n",
@@ -78,7 +78,7 @@ func TestAST2(t *testing.T) {
 		"<p><em>What is A* algorithm?</em></p>\n",
 
 		"## *Emphasised* header\n> quote",
-		"<h2><em>Emphasised</em> header</h2>\n\n<blockquote>\n<p>quote</p>\n</blockquote>\n",
+		"<h2><em>Emphasised</em> header</h2>\n<blockquote>\n<p>quote</p>\n</blockquote>\n",
 
 		// hard line break
 		"foo  \nbar",
@@ -145,12 +145,6 @@ func TestAST2(t *testing.T) {
 		"<p><a href=\"url\">link</a></p>\n",
 	}
 	var candidate string
-	// catch and report panics
-	defer func() {
-		if err := recover(); err != nil {
-			t.Errorf("\npanic while processing [%#v]: %s\n", candidate, err)
-		}
-	}()
 	for i := 0; i+1 < len(tests); i += 2 {
 		input := tests[i]
 		candidate = input
@@ -159,6 +153,102 @@ func TestAST2(t *testing.T) {
 		renderer := HtmlRenderer(UseXHTML, "", "")
 		Markdown([]byte(input), renderer, NoExtensions)
 		actual := string(render_CommonMark(renderer.GetAST()))
+		if actual != expected {
+			t.Errorf("\nInput   [%#v]\nExpected[%#v]\nActual  [%#v]",
+				candidate, expected, actual)
+		}
+	}
+}
+
+func TestRepro(t *testing.T) {
+	var tests = []string{
+		//"Hello\n# Header 1\nGoodbye\n",
+		//"<p>Hello</p>\n\n<h1>Header 1</h1>\n\n<p>Goodbye</p>\n",
+		//"* Hello\n",
+		//"<ul>\n<li>Hello</li>\n</ul>\n",
+		"# Header 1",
+		"<h1>Header 1</h1>\n",
+
+		"Hello <!-- there -->\n",
+		"<p>Hello <!-- there --></p>\n",
+
+		"<!-- block comment -->\n\ntext",
+		"<!-- block comment -->\n<p>text</p>\n",
+
+		"<hr />\n\ntext",
+		"<hr />\n<p>text</p>\n",
+
+		"<div></div>\n",
+		"<div></div>\n",
+
+		"# Header 1\n\n----\n",
+		"<h1>Header 1</h1>\n\n<hr />\n",
+
+		"# Header 1\n\n----\n\n> quote",
+		"<h1>Header 1</h1>\n\n<hr />\n\n<blockquote>\n<p>quote</p>\n</blockquote>\n",
+
+		"    Code\n========\n",
+		"<pre><code>Code\n</code></pre>\n\n<p>========</p>\n",
+
+		"``` go\nfunc foo() bool {\n\treturn true;\n}\n```\n",
+		"<pre><code class=\"language-go\">func foo() bool {\n\treturn true;\n}\n</code></pre>\n",
+
+		"* Yin\n* Yang\n",
+		"<ul>\n<li>Yin</li>\n<li>Yang</li>\n</ul>\n",
+	}
+	var candidate string
+	for i := 0; i+1 < len(tests); i += 2 {
+		input := tests[i]
+		candidate = input
+		expected := tests[i+1]
+		renderer := HtmlRenderer(UseXHTML, "", "")
+		actual := string(Markdown([]byte(input), renderer, FencedCode))
+		//Markdown([]byte(input), renderer, FencedCode)
+		//actual := string(render_Blackfriday(renderer.GetAST()))
+		if actual != expected {
+			t.Errorf("\nInput   [%#v]\nExpected[%#v]\nActual  [%#v]",
+				candidate, expected, actual)
+		}
+	}
+}
+*/
+
+func TestRepro2(t *testing.T) {
+	cat := func(s ...string) string { return strings.Join(s, "\n") }
+	var tests = []string{
+		//"> foo\n>\n> > bar\n>\n> foo\n",
+		//"<blockquote>\n<p>foo</p>\n\n<blockquote>\n<p>bar</p>\n</blockquote>\n\n<p>foo</p>\n</blockquote>\n",
+
+		//"> Blockquoted: <http://example.com/>\n\nnah: `<http://example.com/>`\n",
+		//"<blockquote>\n<p>Blockquoted: <a href=\"http://example.com/\">http://example.com/</a></p>\n</blockquote>\n\n<p>nah: <code>&lt;http://example.com/&gt;</code></p>\n",
+
+		cat("it easy to use Markdown to write about HTML example code:",
+			"",
+			"    I strongly recommend against using any `<blink>` tags.",
+			"",
+			"    I wish SmartyPants used named entities like `&mdash;`",
+			"    instead of decimal-encoded entites like `&#8212;`.",
+			"",
+			"Output:"),
+		cat("<p>it easy to use Markdown to write about HTML example code:</p>",
+			"",
+			"<pre><code>I strongly recommend against using any `&lt;blink&gt;` tags.",
+			"",
+			"I wish SmartyPants used named entities like `&amp;mdash;`",
+			"instead of decimal-encoded entites like `&amp;#8212;`.",
+			"</code></pre>",
+			"",
+			"<p>Output:</p>",
+			""),
+	}
+	var candidate string
+	for i := 0; i+1 < len(tests); i += 2 {
+		input := tests[i]
+		candidate = input
+		expected := tests[i+1]
+		renderer := HtmlRenderer(UseXHTML|UseSmartypants|SmartypantsDashes, "", "")
+		actual := string(Markdown([]byte(input), renderer, FencedCode|Strikethrough|BackslashLineBreak|Autolink))
+		//actual := string(render_Blackfriday(renderer.GetAST()))
 		if actual != expected {
 			t.Errorf("\nInput   [%#v]\nExpected[%#v]\nActual  [%#v]",
 				candidate, expected, actual)
