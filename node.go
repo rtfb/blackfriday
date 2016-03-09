4e095ec36222d1b5daed3911732f93872e848f18
Vytautas Šaltenis <vytas@rtfb.lt>
2016-03-09 12:13:28 +0200

    Move remaining ast_block.go code to node.go

--- node.go
+++ node.go
@@ -55,6 +55,25 @@ func (t NodeType) String() string {
 	return nodeTypeNames[t]
 }
 
+// XXX: there's already ListType in blackfriday, so name it somewhat
+// differently for now. See if both types are necessary later.
+type ASTListType int
+
+const (
+	BulletList ASTListType = iota
+	OrderedList
+)
+
+type ListData struct {
+	Type         ASTListType
+	tight        bool // skip <p>s around list item data if true
+	bulletChar   byte // '*', '+' or '-' in bullet lists
+	start        uint32
+	delimiter    byte // '.' or ')' after the number in ordered lists
+	padding      uint32
+	markerOffset uint32
+}
+
 type Node struct {
 	Type          NodeType
 	parent        *Node
